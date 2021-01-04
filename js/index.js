52973b596f3cf52aea211d5127f3b45bc2d85718
Luis Mendoza <lmendoza.mg@gmail.com>
2021-01-04 13:58:43 +0100

    MediaPlayer module created and linked

--- js/index.js
+++ js/index.js
@@ -1,21 +1,7 @@
-
-function MediaPlayer (config) {
-  this.media = config.el;
-}
-
-MediaPlayer.prototype.play = function() {
-  if(this.media.paused){          
-    this.media.play();
-  }else{
-    this.media.pause();
-  }
-};
+import MediaPlayer from './MediaPlayer.js';
 
 const video = document.querySelector("video");
 const player = new MediaPlayer({ el: video });
 
 const button = document.querySelector("button");
 button.onclick = () => player.play();
-
-
-
